46e3116d7789bdf15b47015bba9609456f252bc0
Anchal142 <anchalsingh1029@gmail.com>
2023-02-01 11:23:15 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'pages/splash_screen.dart';
+import 'firebase_options.dart';
+
+void main() {
+  runApp(LoginUiApp());
+}
+
+class LoginUiApp extends StatelessWidget {
+  Color _primaryColor = HexColor('#DC54FE');
+  Color _accentColor = HexColor('#8A02AE');
+
+  // Design color
+  // Color _primaryColor= HexColor('#FFC867');
+  // Color _accentColor= HexColor('#FF3CBD');
+
+  // Our Logo Color
+  // Color _primaryColor= HexColor('#D44CF6');
+  // Color _accentColor= HexColor('#5E18C8');
+
+  // Our Logo Blue Color
+  //Color _primaryColor= HexColor('#651BD2');
+  //Color _accentColor= HexColor('#320181');
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Login UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: _primaryColor,
+        accentColor: _accentColor,
+        scaffoldBackgroundColor: Colors.grey.shade100,
+        primarySwatch: Colors.grey,
+      ),
+      home: SplashScreen(title: 'Flutter Login UI'),
+    );
+  }
+}
